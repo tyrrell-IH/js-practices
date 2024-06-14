4ce728f0e0b6167b3cbf9055be061e96757a2e34
tyrrell-IH <gotothertm@gmail.com>
2024-06-14 14:35:04 +0900

    定数名LAST_NUMBERがFIRST_NUMBER(最初の数)の存在を連想させるので名称変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const LAST_NUMBER = 20;
+const MAX_COUNT = 20;
 
-for (let count = 1; count <= LAST_NUMBER; count++) {
+for (let count = 1; count <= MAX_COUNT; count++) {
   if (count % 3 === 0 && count % 5 === 0) {
     console.log("FizzBuzz");
   } else if (count % 3 === 0) {
