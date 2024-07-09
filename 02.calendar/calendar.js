c44f42a410415a200185e6e148d40b2b31307cc3
tyrrell-IH <gotothertm@gmail.com>
2024-07-09 11:58:31 +0900

    可読性の観点からDateオブジェクト作成の際に第3引数を省略せず明示するよう修正

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -7,7 +7,7 @@ const main = () => {
   const argv = minimist(process.argv.slice(2));
   const year = argv["y"] || today.getFullYear();
   const month = argv["m"] || today.getMonth() + 1;
-  const firstDay = new Date(year, month - 1);
+  const firstDay = new Date(year, month - 1, 1);
   const lastDay = new Date(year, month, 0);
   displayHeader(year, month);
   displayBlankSpaces(firstDay);
