388999520929b815475136dbfe9b3883ca20450f
tyrrell-IH <gotothertm@gmail.com>
2024-09-02 15:49:20 +0900

    Promiseを使用したnode-sqlite3の操作（エラーなし版)を実装

---
+++ 03.asynchronous/using-sqlite3-with-promise.js
@@ -0,0 +1,33 @@
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database(":memory");
+
+const runPromise = (db, sqlStatement) => {
+  return new Promise((resolve) => {
+    db.run(sqlStatement, function () {
+      resolve(this);
+    });
+  });
+};
+
+const getPromise = (db, sqlStatement) => {
+  return new Promise((resolve) => {
+    db.get(sqlStatement, (err, row) => {
+      resolve(row);
+    });
+  });
+};
+
+runPromise(
+  db,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE )",
+)
+  .then(() => runPromise(db, "INSERT INTO books(title) values ('蟹工船')"))
+  .then((value) => {
+    console.log(value.lastID);
+    return getPromise(db, "SELECT * FROM books WHERE title = '蟹工船' ");
+  })
+  .then((row) => {
+    console.log(`id: ${row.id} title: ${row.title}`);
+  })
+  .then(() => runPromise(db, "DROP TABLE books"));
