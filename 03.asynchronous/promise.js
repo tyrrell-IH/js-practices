bd2d7939f30a920637feda8cd3575fbecc9fe383
tyrrell-IH <gotothertm@gmail.com>
2024-09-10 11:10:37 +0900

    ベタ書きで実行していた箇所を関数の形式に修正しました

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -4,26 +4,32 @@ import { runPromise, getPromise } from "./promise-based-function.js";
 
 const db = new sqlite3.Database(":memory");
 
-runPromise(
-  db,
-  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE )",
-)
-  .then(() => runPromise(db, "INSERT INTO books(title) values ('蟹工船')"))
-  .then((value) => {
-    console.log(`id: ${value.lastID}`);
-    return getPromise(db, "SELECT * FROM books WHERE title = '蟹工船' ");
-  })
-  .then((row) => console.log(`id: ${row.id} title: ${row.title}`))
-  .then(() => runPromise(db, "DROP TABLE books"));
+const runWithoutError = (db) => {
+  runPromise(
+    db,
+    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE )",
+  )
+    .then(() => runPromise(db, "INSERT INTO books(title) values ('蟹工船')"))
+    .then((value) => {
+      console.log(`id: ${value.lastID}`);
+      return getPromise(db, "SELECT * FROM books WHERE title = '蟹工船' ");
+    })
+    .then((row) => console.log(`id: ${row.id} title: ${row.title}`))
+    .then(() => runPromise(db, "DROP TABLE books"));
+};
 
+const runWithError = (db) => {
+  runPromise(
+    db,
+    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE )",
+  )
+    .then(() => runPromise(db, "INSERT INTO books(title) values (null)"))
+    .catch((error) => console.log(error.message))
+    .then(() => getPromise(db, "SELECT * FROM bookoff "))
+    .catch((error) => console.log(error.message))
+    .finally(() => runPromise(db, "DROP TABLE books"));
+};
+
+runWithoutError(db);
 await timers.setTimeout(100);
-
-runPromise(
-  db,
-  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE )",
-)
-  .then(() => runPromise(db, "INSERT INTO books(title) values (null)"))
-  .catch((error) => console.log(error.message))
-  .then(() => getPromise(db, "SELECT * FROM bookoff "))
-  .catch((error) => console.log(error.message))
-  .finally(() => runPromise(db, "DROP TABLE books"));
+runWithError(db);
