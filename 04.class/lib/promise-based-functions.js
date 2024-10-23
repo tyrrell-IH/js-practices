d66665e820a59408d28b8ed76f8bcaccfac19c44
tyrrell-IH <gotothertm@gmail.com>
2024-10-23 11:58:19 +0900

    db操作の関数を、独自にPromiseでラップするのではなくpromisifyを使ってラップする方法に変更

--- 04.class/lib/promise-based-functions.js
+++ 04.class/lib/promise-based-functions.js
@@ -1,47 +1,21 @@
-export const run = (db, sqlQuery, params = []) => {
-  return new Promise((resolve, reject) => {
-    db.run(sqlQuery, params, function (error) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(this);
-      }
-    });
-  });
-};
+import * as util from "node:util";
 
-export const get = (db, sqlQuery, params = []) => {
-  return new Promise((resolve, reject) =>
-    db.get(sqlQuery, params, function (error, row) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(row);
-      }
-    }),
-  );
-};
+export async function run(db, sqlQuery, params = []) {
+  const promiseRun = util.promisify(db.run).bind(db);
+  return await promiseRun(sqlQuery, params);
+}
 
-export const all = (db, sqlQuery) => {
-  return new Promise((resolve, reject) =>
-    db.all(sqlQuery, function (error, row) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(row);
-      }
-    }),
-  );
-};
+export async function get(db, sqlQuery, params = []) {
+  const promiseGet = util.promisify(db.get).bind(db);
+  return await promiseGet(sqlQuery, params);
+}
 
-export const close = (db) => {
-  return new Promise((resolve, reject) =>
-    db.close((error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve();
-      }
-    }),
-  );
-};
+export async function all(db, sqlQuery) {
+  const promiseAll = util.promisify(db.all).bind(db);
+  return await promiseAll(sqlQuery);
+}
+
+export async function close(db) {
+  const promiseClose = util.promisify(db.close).bind(db);
+  return await promiseClose();
+}
